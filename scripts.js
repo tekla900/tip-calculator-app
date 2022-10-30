cfeace8c90e6ecee8286418de4d28a43722ed6c2
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-10-30 22:06:57 +0400

    added validation and marked active tips

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+let allInputs = document.querySelectorAll("#bill, #people");
 let billInput = document.getElementById('bill');
 let personInput = document.getElementById('people');
 let procents = document.querySelectorAll('button.percent');
@@ -7,46 +8,68 @@ let tipAmount = document.getElementById('tip-amount');
 let totalAmount = document.getElementById('total');
 
 let resetBtn = document.querySelector('.reset')
+let errorSpan = document.querySelector('.error-span');
 
-let tip_percentage;
+let tipPercentage;
 
 resetBtn.disabled = true;
-// let allInputs = document.querySelectorAll('span.test:not(.asd)');
-let allInputs = document.querySelectorAll("#bill, #people");
 
-allInputs.forEach(element => {
-    element.addEventListener('input', () => displayTotals());
-});
-
-procents.forEach(each => {
-    each.addEventListener('click', () => {
-        tip_percentage = Number(each.value/100);
-        displayTotals();
-    });
-});
-
-customTip.addEventListener('input', () => {
-    tip_percentage = Number(customTip.value/100);
+function reset() {
+    billInput.value = 0;
+    personInput.value = 0;
+    tipPercentage = 0;
     displayTotals();
-});
+};
 
-// billInput.addEventListener('input', () => {
-//     displayTotals();
-// });
-
-// personInput.addEventListener('input', () => {
-//     displayTotals();
-// });
+function tipsAddActive(event) {
+    procents.forEach(each => {
+        each.classList.remove('active');
+        if(event.target.textContent == each.textContent) {
+            each.classList.add("active");
+            tipPercentage = Number(each.value/100);
+            displayTotals();
+        };
+    });
+};
 
 function displayTotals() {
-    if (tip_percentage && billInput.value && personInput.value) {
-        let tip = (billInput.value * tip_percentage / personInput.value);
+    if (tipPercentage && billInput.value && personInput.value) {
+        let tip = (billInput.value * tipPercentage / personInput.value);
         let total = ((Number(billInput.value) + tip)/ personInput.value).toFixed(2);
         totalAmount.textContent = `$${total}`;
         tipAmount.textContent = `$${tip.toFixed(2)}`;
 
         resetBtn.disabled = false;
-    };  
+    }
+    else {
+        tipAmount.textContent = "$0.00";
+        totalAmount.textContent = "$0.00";
+        resetBtn.disabled = true;
+    };
 };
 
-// displayTotals();
+procents.forEach(each => {
+    each.addEventListener('click', tipsAddActive);
+});
+
+customTip.addEventListener('input', () => {
+    tipPercentage = Number(customTip.value/100);
+    displayTotals();
+});
+
+personInput.addEventListener('input', () => {
+    if (personInput.value < 1 || personInput.value % 1 != 0) {
+        personInput.style.borderColor = '#E17052';
+        errorSpan.style.display = 'block';
+    }
+    else {
+        personInput.style.border = 'none';
+        errorSpan.style.display = 'none';
+    };
+});
+
+allInputs.forEach(element => {
+    element.addEventListener('input', () => displayTotals());
+});
+
+resetBtn.addEventListener('click', () => reset());
